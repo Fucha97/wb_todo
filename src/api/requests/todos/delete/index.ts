9168c012ac72688ad370a07a2d466a8598e522f6
Fucha <egor.fucha@gmail.com>
2023-10-02 15:45:20 +0300

    [fix]: Fixed mock server

--- src/api/requests/todos/delete/index.ts
+++
@@ -1,7 +0,0 @@
-import { IResponse, RestRequest } from '@mihanizm56/fetch-api';
-import { DeleteTaskParamsType, makeRequestConfig } from './make-request-config';
-
-export const deleteTodoRequest = (
-  params: DeleteTaskParamsType,
-): Promise<IResponse> =>
-  new RestRequest().deleteRequest(makeRequestConfig(params));
