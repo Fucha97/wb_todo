637e1b078b3c4098acc1d8ff998f6e5129ee11aa
Fucha <egor.fucha@gmail.com>
2023-10-02 05:05:07 +0300

    [feat]: Created special connect component

---
+++ src/pages/todos/_redux/todo-module/reducer.ts
@@ -0,0 +1,36 @@
+import { Reducer } from 'redux';
+import {
+  ETodosActions,
+  TodoStateType,
+} from '@/pages/todos/_redux/todo-module/_types';
+
+export const initialTodoSlice: TodoStateType = {
+  isLoading: false,
+  todos: [],
+  updateTodoId: null,
+};
+
+export const todoReducer: Reducer<TodoStateType> = (
+  state: TodoStateType = initialTodoSlice,
+  { type, payload },
+) => {
+  switch (type) {
+    case ETodosActions.SET_TODOS_LOADING:
+      return {
+        ...state,
+        isLoading: payload,
+      };
+    case ETodosActions.SET_TODOS:
+      return {
+        ...state,
+        todos: payload,
+      };
+    case ETodosActions.SET_UPDATE_TODO_ID:
+      return {
+        ...state,
+        updateTodoId: payload,
+      };
+    default:
+      return state;
+  }
+};
